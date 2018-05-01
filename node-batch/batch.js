d2fd564b3511ad60d4745d4a46e30d9c2ba29feb
Sichen Liu <sl2947@cornell.edu>
2018-05-01 14:50:49 -0400

    Add batch work

--- node-batch/batch.js
+++ node-batch/batch.js
@@ -3,28 +3,52 @@ var request = require('request')
 // Redirect text:
 //       http://localhost:8082/?request=dog.ceo/api/breeds/image/random
 
-// Problem -> node mode is single thread, so it cannot handle async model easily.
-
+// Bash and run request. We need sync-calls to run jobs.
+var exec = require('sync-exec')
 var count = 0;
-var rqueue = [];
+var rqueue = [];    // Store the requests.
+
+function sort_requests(request_queue){
+    // Request_queue: {"URL": frequency}
+    request_queue.sort();
+    res = {};
+    for(i=0;i < request_queue.length; i++){
+        if (request_queue[i] in res){
+            res[request_queue[i]] += 1;
+        }
+        else{
+            res[request_queue[i]] = 1;
+        }
+    }
+    return res;
+}
+
+function make_requests(req_list, count){
+    // Measurement the cost time.
+    console.time('test');
+    for (var key in req_list){
+        cmd = "hey -c 10 -n " + req_list[key] + " " + key; 
+        exec(cmd);
+    }
+    console.timeEnd('test');
+}   
+
 http.createServer(function (req, res) {
     if (req.url.slice(2,9) == "request"){
             count += 1;
-            console.log(count);
             var replay = req.url.slice(10,);
             var newurl = 'https://' + replay;
             rqueue.push(newurl);
-                if (count == 3) {
-                    // 100s requests
-                    console.log(rqueue[0])
-                    count -= 1;
-                    for(var i=0; i<3; i++){
-                        request(rqueue[i], function(error, response, body){
-                            res.writeHead(200, {'Content-Type': 'text/plain'});
-                            res.end(body);
-                        })}
-                        rqueue = [];
-                    console.log("out")
-                    }
+            if (count == 20) {
+            // 100s requests
+            rqueue = sort_requests(rqueue, count);
+            console.log(rqueue);
+            make_requests(rqueue);
+            rqueue = [];
+            count = 0
         }
+    }
+    res.writeHead(200, {'Context-Type': 'text/plain'})
+    res.end("body");
+
 }).listen(8082);
---
+++ node-batch/readme.md
@@ -0,0 +1,13 @@
+### Batch requests
+According to tests, the interlevaing between containers or function inside same container will improve the costs. To accelerate this process, we use batch technology to handle requests.
+
+#### Package install
+```
+npm install sync-exec
+npm install hey
+```
+
+#### Testing command line
+```
+
+```
