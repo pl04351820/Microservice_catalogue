1cd735aee38a11d3ca20efad838596b56f09379e
Sichen Liu <sl2947@cornell.edu>
2018-05-03 16:50:09 -0400

    Add change

--- separate-catalogue/tag-page.js
+++ separate-catalogue/tag-page.js
@@ -1,6 +1,12 @@
+var tags = {"tags":["brown","geek","formal","blue","skin","red","action","sport","black","magic","green"],"err":null}
+
 module.exports = async function(context) {
     return {
         status: 200,
-        body: JSON.stringify({"tags":["brown","geek","formal","blue","skin","red","action","sport","black","magic","green"],"err":null})
+        body: JSON.stringify(tags)
     };
 }
+
+
+
+
